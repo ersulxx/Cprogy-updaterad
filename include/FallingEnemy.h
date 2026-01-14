1bf935e5a7f566a67dc9b6aae3bb56de81eadfd4
Enes <enesihsancelik@gmail.com>
2026-01-14 18:53:02 +0100

    Added comments in FallingEnemy

--- include/FallingEnemy.h
+++ include/FallingEnemy.h
@@ -2,12 +2,16 @@
 #include "MoveableSprite.h"
 #include <string>
 
+/* FallingEnemy är en specifik typ av MoveableSprite som rör
+sig nedåt på skärmen och kan orsaka att spelet tar slut */
 class FallingEnemy : public demo::MoveableSprite
 {
 
 public:
+    // Konstruktorn som tar emot bildsökväg, startposition och fall hastighet
     FallingEnemy(const std::string &img, float x, float y, float speed);
     void tick() override;
+    // Hanterar vad som händer vid krock
     void onCollisionWith(demo::SpritePtr other) override;
     bool canTriggerGameOver() const override;
 
